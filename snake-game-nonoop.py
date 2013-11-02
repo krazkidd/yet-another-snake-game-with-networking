a4e2e916d95ae948fcbbbded40613606afe70df0
mark <mark@19ec2af5-9665-1f45-cda8-470fbe4bc1ed>
2013-11-02 19:19:06 +0000

    Added non-OOP version of game. Added ability to quit with ESC.

--- snake-game-nonoop.py
+++ snake-game-nonoop.py
@@ -1,39 +1,22 @@
 #!/usr/bin/python2
 
 import curses
+import curses.ascii
 import time
-from Snake import Dir
+from Snakenonoop import *
 
-# player's snake
-snakeHeadX = 15
-snakeHeadY = 15
-snakeHeading = Dir.Right
-snakeLength = 4
-
-def moveSnake():
-	"""Move (update) the snake's body.
-
-	This should be called once for every unit
-	of time that passes."""
-	global snakeHeadX, snakeHeadY, snakeHeading, snakeLength
-
-	# check the heading of the snake and move the
-	# head's position accordingly
-	if snakeHeading == Dir.Right:
-		snakeHeadX += 1
-	elif snakeHeading == Dir.Left:
-		snakeHeadX -= 1
-	elif snakeHeading == Dir.Up:
-		snakeHeadY -= 1
-	else: # snakeHeading == Dir.Down
-		snakeHeadY += 1
+# make snakes 
+# format (see SnakeEnum): [length, heading, headX, headY]
+snake1 = [4, Dir.Right, 15, 15]
+snake2 = [4, Dir.Right, 30, 30]
 
 # initiate curses
 stdscr = curses.initscr() # get window object for whole screen
 curses.noecho() # turn off character echo
 curses.cbreak() # get input after every keypress
 curses.start_color() # for color support
-curses.init_pair(1, curses.COLOR_RED, curses.COLOR_BLACK) # define a color for player's snake
+curses.init_pair(1, curses.COLOR_RED, curses.COLOR_BLACK) # define a color for player1's snake
+curses.init_pair(2, curses.COLOR_GREEN, curses.COLOR_BLACK) # define a color for player2's snake
 stdscr.keypad(1) # return special keys as single chars
 stdscr.nodelay(True)
 curses.curs_set(0) # make cursor invisible
@@ -47,8 +30,10 @@ while True:
 	stdscr.border()
 
 	# draw snake
-	stdscr.addch(snakeHeadY, snakeHeadX, 'O', curses.color_pair(1))
+	stdscr.addch(snake1[SnakeEnum.HeadY], snake1[SnakeEnum.HeadX], 'O', curses.color_pair(1))
+	stdscr.addch(snake2[SnakeEnum.HeadY], snake2[SnakeEnum.HeadX], 'O', curses.color_pair(2))
 
+	# actually paint the window
 	stdscr.refresh()
 
 	# get user input, if any
@@ -56,20 +41,23 @@ while True:
 
 	# change the heading of the snake according to user input
 	if input == curses.KEY_UP or input == ord('w'):
-		snakeHeading = Dir.Up
+		snake1[SnakeEnum.Heading] = Dir.Up
 	elif input == curses.KEY_DOWN or input == ord('s'):
-		snakeHeading = Dir.Down
+		snake1[SnakeEnum.Heading] = Dir.Down
 	elif input == curses.KEY_LEFT or input == ord('a'):
-		snakeHeading = Dir.Left
+		snake1[SnakeEnum.Heading] = Dir.Left
 	elif input == curses.KEY_RIGHT or input == ord('d'):
-		snakeHeading = Dir.Right
+		snake1[SnakeEnum.Heading] = Dir.Right
+	elif input == curses.ascii.ESC: # quit when Escape is pressed
+		break
 
-	# move player's snake
-	moveSnake()
+	# move players' snakes
+	moveSnake(snake1)
+	moveSnake(snake2)
 
 	# pause the screen for just a bit
 	time.sleep(0.2)
-
+	
 # terminate curses
 curses.nocbreak(); stdscr.keypad(0); curses.echo()
 curses.endwin()
--- snake-game.py
+++ snake-game.py
@@ -1,22 +1,25 @@
 #!/usr/bin/python2
 
 import curses
+import curses.ascii
 import time
 from Snake import *
+
+# instantiate players' snakes
+snake1 = Snake(15, 15, Dir.Right, 4)
+#snake2 = Snake(30, 30, Dir.Right, 4)
+
 
 # initiate curses
 stdscr = curses.initscr() # get window object for whole screen
 curses.noecho() # turn off character echo
 curses.cbreak() # get input after every keypress
 curses.start_color() # for color support
-curses.init_pair(1, curses.COLOR_RED, curses.COLOR_BLACK) # define a color for player's snake
+curses.init_pair(1, curses.COLOR_RED, curses.COLOR_BLACK) # define a color for player1's snake
+curses.init_pair(2, curses.COLOR_GREEN, curses.COLOR_BLACK) # define a color for player2's snake
 stdscr.keypad(1) # return special keys as single chars
 stdscr.nodelay(True)
 curses.curs_set(0) # make cursor invisible
-
-# instantiate player's snake
-snake = Snake(15, 15, Dir.Right, 4)
-#snake2 = Snake()
 
 # main game loop
 while True:
@@ -26,10 +29,11 @@ while True:
 	# draw outside border
 	stdscr.border()
 
-	# draw snake
-	stdscr.addch(snake.headY, snake.headX, 'O', curses.color_pair(1))
-	#stdscr.addch(snake2.headY, snake2.headX, 'O', curses.COLOR_GREEN)
+	# draw snakes
+	stdscr.addch(snake1.headY, snake1.headX, 'O', curses.color_pair(1))
+	#stdscr.addch(snake2.headY, snake2.headX, 'O', curses.color_pair(2))
 
+	# actually paint the window
 	stdscr.refresh()
 
 	# get user input, if any
@@ -37,18 +41,20 @@ while True:
 
 	# change the heading of the snake according to user input
 	if input == curses.KEY_UP or input == ord('w'):
-		snake.heading = Dir.Up
+		snake1.heading = Dir.Up
 	elif input == curses.KEY_DOWN or input == ord('s'):
-		snake.heading = Dir.Down
+		snake1.heading = Dir.Down
 	elif input == curses.KEY_LEFT or input == ord('a'):
-		snake.heading = Dir.Left
+		snake1.heading = Dir.Left
 	elif input == curses.KEY_RIGHT or input == ord('d'):
-		snake.heading = Dir.Right
+		snake1.heading = Dir.Right
+	elif input == curses.ascii.ESC: # quit when Escape is pressed
+		break
 
-	# move player's snake
-	snake.move()
+	# move players' snakes
+	snake1.move()
+	#Snake.move(snake1) # an alternative way to call a particular object's method
 	#snake2.move()
-	#Snake.move(snake) # an alternative way to call a particular object's method
 
 	# pause the screen for just a bit
 	time.sleep(0.2)
