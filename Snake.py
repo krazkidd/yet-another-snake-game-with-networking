5beda668628ce01e46e4d70a5bf706afdc8b6a35
mark <mark@19ec2af5-9665-1f45-cda8-470fbe4bc1ed>
2013-11-23 01:52:47 +0000

    Moved Pellet back to Snake.py, so it's all in the same module.

--- Snake.py
+++ Snake.py
@@ -1,11 +1,24 @@
 from collections import deque
 import random
 
-from Pellet import *
-
 class Dir:
 	"""An enum of the cardinal directions."""
 	Up, Down, Left, Right = range(4)
+
+class Pellet:
+
+	"""This is what the Snake eats.
+
+	For now, this just holds a position. Other attributes could
+	be added in the future like power-up abilities."""
+
+	def __init__(self, stdscr_x, stdscr_y):
+		
+		"""On creation, randomly set a position using the
+		given arguments as maximum values."""
+
+		self.posx = random.randint(0, stdscr_x - 1) # subtract 1 because cursor position is zero-based
+		self.posy = random.randint(0, stdscr_y - 1)
 
 class Snake:
 
