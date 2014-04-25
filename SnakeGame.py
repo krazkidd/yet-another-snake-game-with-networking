6bf32876c93dfb346dba67041ae36e7ab360f891
mark <mark@19ec2af5-9665-1f45-cda8-470fbe4bc1ed>
2014-04-25 16:08:55 +0000

    Added SnakeGame class. Among other benefits, this will make it easier to add new gametypes!

---
+++ SnakeGame.py
@@ -0,0 +1,100 @@
+import os
+import sys
+
+import pygame
+from pygame.locals import *
+import pygcurse
+
+from Snake import *
+
+class SnakeGame:
+    def __init__(self, winWidth, winHeight):
+        self.winWidth = winWidth
+        self.winHeight = winHeight
+
+        # instantiate players' snakes
+        self.snake1 = Snake(15, 15, Dir.Right, 4)
+        self.snakeAI = SnakeAI(30, 30, Dir.Right, 4)
+
+        self.pellet = Pellet(winWidth - 1, winHeight - 1, 'yellow') 
+
+        # initiate pygame and pygcurse
+        os.environ['SDL_VIDEO_CENTERED'] = '1' # center window in Windows
+        pygame.init()
+        self.win = pygcurse.PygcurseWindow(winWidth, winHeight, 'Snake')
+        self.win.autoupdate = False # turn off autoupdate so window doesn't flicker
+
+    def processInput(self):
+        # process input queue
+        for event in pygame.event.get():
+            if event.type == QUIT or (event.type == KEYDOWN and event.key == K_ESCAPE):
+                pygame.quit()
+                sys.exit()
+            if event.type == KEYDOWN:
+                if event.key == K_UP:
+                    self.snake1.changeHeading(Dir.Up)
+                elif event.key == K_DOWN:
+                    self.snake1.changeHeading(Dir.Down)
+                elif event.key == K_LEFT:
+                    self.snake1.changeHeading(Dir.Left)
+                elif event.key == K_RIGHT:
+                    self.snake1.changeHeading(Dir.Right)
+
+    def tick(self):
+        # move players' snakes
+        self.snake1.move(self.pellet)
+        #Snake.move(snake1) # an alternative way to call a particular object's method
+        self.snakeAI.move(self.pellet)    
+        
+        # check if player's head is on a pellet. If so, consume it and create a new one
+        if self.snake1.headX == self.pellet.posx and self.snake1.headY == self.pellet.posy:
+            self.snake1.length += 1
+            self.pellet = Pellet(self.winWidth - 1, self.winHeight - 1, fgcolor = 'yellow')
+            self.snake1.grow()
+            #s.sendto('Player 1 score:' + str(self.snake1.length), (HOST, lobbyPort))
+        # (SnakeAI) check if player's head is on a pellet. If so, consume it and create a new one
+        elif self.snakeAI.headX == self.pellet.posx and self.snakeAI.headY == self.pellet.posy:
+            self.snakeAI.length += 1
+            self.pellet = Pellet(self.winWidth - 1, self.winHeight - 1, 'yellow')
+            self.snakeAI.grow()
+            #s.sendto('SnakeAI score:' + str(self.snakeAI.length), (HOST, lobbyPort))
+
+        # check if any snakes are colliding with any other snakes
+        #if self.snake1.isColl((self.snake1.headX, self.snake1.headY)): # previous use of Snake.isColl()
+        if self.snake1.isColl(self.snake1):
+            print 'snake1 bit itself'     
+        elif self.snake1.isColl(self.snakeAI):
+            print 'snake1 ran into the other snake'     
+
+        if self.snakeAI.isColl(self.snakeAI):
+            print 'snakeAI bit itself'
+        elif self.snakeAI.isColl(self.snake1):
+            print 'snakeAI ran into the other snake'     
+
+        #TODO check if any snakes have hit the edge
+        #if ...
+
+    def drawWindow(self):
+        # clear the screen
+        #win.erase() # this would be used instead but for a bug...
+        self.win.fill(' ')
+
+        # draw outside border
+        #TODO
+
+        # draw game data
+        self.win.putchars("Score: " + str(self.snake1.length), 0, 0)
+
+        #FIXME draw snakes and pellet (get positions from class)
+        for pos in self.snake1.body:
+            # pos is a tuple (x, y)
+            self.win.putchars('O', pos[0], pos[1], fgcolor = self.snake1.fgcolor)
+
+        for pos in self.snakeAI.body:
+            self.win.putchars('O', pos[0], pos[1], self.snakeAI.fgcolor)
+
+        # draw pellet
+        self.win.putchar('+', self.pellet.posx, self.pellet.posy, self.pellet.fgcolor)
+
+        # actually paint the window
+        self.win.update()
--- snake-game.py
+++ snake-game.py
@@ -3,7 +3,6 @@
 #TODO add copyright and license info
 
 import time
-import sys
 import os 
 from os.path import basename
 import socket
@@ -11,11 +10,8 @@ from struct import pack
 from struct import unpack
 from struct import calcsize
 
-import pygame
-from pygame.locals import *
-import pygcurse
-
-from Snake import *
+#from Snake import *
+from SnakeGame import *
 
 #FIXME allow host and port config from command line. what if port is appended to hostname?
 HOST = '' # any network interface
@@ -29,8 +25,10 @@ def main():
     # get program name and fork to server or client mode
     if 'snakes' == basename(sys.argv[0]):
         doMainServerStuff()
-    if 'snake' == basename(sys.argv[0]):
+    elif 'snake' == basename(sys.argv[0]):
         doClientStuff()
+    else:
+        print 'Usage: This program must be called via symlink with name `snake` (client) or `snakes` (server).'
 
 def doMainServerStuff():
     connectNum = 1
@@ -95,9 +93,10 @@ def doClientStuff():
     s.connect((HOST, PORT))
     print 'Connected to server.'
 
-    #FIXME get lobby info
+    # get lobby info
     lobbyPort = unpack(STRUCT_FMT_LOBBY, s.recv(calcsize(STRUCT_FMT_LOBBY)))[0]
-    print 'Joining lobby number ' + str(lobbyPort)
+    #FIXME client sees port number, not actual lobby number. Subtract PORT here or tell client their client num?
+    print 'Joining lobby number ' + str(lobbyPort) + '.'
 
     # move to UDP port for game
     s.shutdown(socket.SHUT_RDWR)
@@ -107,95 +106,19 @@ def doClientStuff():
     # these dimension units are in text cells, not pixels
     WIN_WIDTH, WIN_HEIGHT = 60, 35
 
-    # instantiate players' snakes
-    snake1 = Snake(15, 15, Dir.Right, 4)
-    snakeAI = SnakeAI(30, 30, Dir.Right, 4)
-
-    pellet = Pellet(WIN_WIDTH - 1, WIN_HEIGHT - 1, 'yellow') 
-
-    # initiate pygame and pygcurse
-    os.environ['SDL_VIDEO_CENTERED'] = '1' # center window in Windows
-    pygame.init()
-    win = pygcurse.PygcurseWindow(WIN_WIDTH, WIN_HEIGHT, 'Snake')
-    win.autoupdate = False # turn off autoupdate so window doesn't flicker
+    game = SnakeGame(WIN_WIDTH, WIN_HEIGHT)
 
     # main game loop
     #FIXME client should go back to lobby after game is over
     while True:
-        # clear the screen
-        #win.erase() # this would be used instead but for a bug...
-        win.fill(' ')
+        game.processInput()
 
-        # draw outside border
-        #TODO
+        game.tick()
 
-        # draw game data
-        win.putchars("Score: " + str(snake1.length), 0, 0)
-
-        # draw snakes
-        for pos in snake1.body:
-            # pos is a tuple (x, y)
-            win.putchars('O', pos[0], pos[1], fgcolor = snake1.fgcolor)
-
-        for pos in snakeAI.body:
-            win.putchars('O', pos[0], pos[1], snakeAI.fgcolor)
-
-        # draw pellet
-        win.putchar('+', pellet.posx, pellet.posy, pellet.fgcolor)
-
-        # actually paint the window
-        win.update()
+        game.drawWindow()
 
         # pause the screen for just a bit
         time.sleep(0.1)
-
-        # process input queue
-        for event in pygame.event.get():
-            if event.type == QUIT or (event.type == KEYDOWN and event.key == K_ESCAPE):
-                pygame.quit()
-                sys.exit()
-            if event.type == KEYDOWN:
-                if event.key == K_UP:
-                    snake1.changeHeading(Dir.Up)
-                elif event.key == K_DOWN:
-                    snake1.changeHeading(Dir.Down)
-                elif event.key == K_LEFT:
-                    snake1.changeHeading(Dir.Left)
-                elif event.key == K_RIGHT:
-                    snake1.changeHeading(Dir.Right)
-
-        # move players' snakes
-        snake1.move(pellet)
-        #Snake.move(snake1) # an alternative way to call a particular object's method
-        snakeAI.move(pellet)    
-        
-        # check if player's head is on a pellet. If so, consume it and create a new one
-        if snake1.headX == pellet.posx and snake1.headY == pellet.posy:
-            snake1.length += 1
-            pellet = Pellet(WIN_WIDTH - 1, WIN_HEIGHT - 1, fgcolor = 'yellow')
-            snake1.grow()
-            s.sendto('Player 1 score:' + str(snake1.length), (HOST, lobbyPort))
-        # (SnakeAI) check if player's head is on a pellet. If so, consume it and create a new one
-        elif snakeAI.headX == pellet.posx and snakeAI.headY == pellet.posy:
-            snakeAI.length += 1
-            pellet = Pellet(WIN_WIDTH - 1, WIN_HEIGHT - 1, 'yellow')
-            snakeAI.grow()
-            s.sendto('SnakeAI score:' + str(snakeAI.length), (HOST, lobbyPort))
-
-        # check if any snakes are colliding with any other snakes
-        #if snake1.isColl((snake1.headX, snake1.headY)): # previous use of Snake.isColl()
-        if snake1.isColl(snake1):
-            print 'snake1 bit itself'     
-        elif snake1.isColl(snakeAI):
-            print 'snake1 ran into the other snake'     
-
-        if snakeAI.isColl(snakeAI):
-            print 'snakeAI bit itself'
-        elif snakeAI.isColl(snake1):
-            print 'snakeAI ran into the other snake'     
-
-        #TODO check if any snakes have hit the edge
-        #if ...
 
 if __name__ == "__main__":
     main()
